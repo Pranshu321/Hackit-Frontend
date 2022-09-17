e457c39de01212a0e6f2c7da8074ecee40de9b14
Mohammad Sami <mohammadsami@duck.com>
2022-09-18 01:10:03 +0530

    Update: SCSS file rename "_" and inport fix

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import "../assets/scss/components/userform.scss"
+// import "../assets/scss/components/userform.scss"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { addDoc, collection } from "firebase/firestore";
