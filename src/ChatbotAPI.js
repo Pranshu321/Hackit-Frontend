97e239a2177d23d90ffe85d7eb3f859b888ee535
Mohammad Sami <mohammadsami@duck.com>
2022-09-18 17:52:08 +0530

    Update: api added

--- src/ChatbotAPI.js
+++ src/ChatbotAPI.js
@@ -1,7 +1,32 @@
 import axios from 'axios'; 
+// import fetch  from 'node-fetch';
 
 async function moodGuesser(message){
-   const res = await axios.post("http://tweetmoodchk.d7cgcshfajbxf6dv.eastus.azurecontainer.io/mood" , {
+
+  // http://tweetmoodchk.d7cgcshfajbxf6dv.eastus.azurecontainer.io/mood
+
+
+
+    // const response = await fetch('http://tweetmoodchk.d7cgcshfajbxf6dv.eastus.azurecontainer.io/mood', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Access-Control-Allow-Origin': '*',
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(
+    //         { "?input_mood=": message }
+    //     ),
+
+    // })
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+    // .catch(error => console.log(error));
+
+    // const data = await response.json();
+    // console.log(data);
+    
+
+   const response = await axios.post("https://cbtest201.azurewebsites.net/mood" , {
     input_mood: message
    } , {
     headers: {
@@ -9,8 +34,17 @@ async function moodGuesser(message){
       'Access-Control-Allow-Origin': '*',
       'content-type': "application/json"
     }
-   });
-   console.log(res.data);
+   })
+    .then(response => response.json())
+    .catch(error => {
+      console.log(error);
+    });
+
+   console.log(response.data);
+   console.log(response.data.name);
+    return response.data.name;
+
+    
 }
 
 const API = {
@@ -20,7 +54,8 @@ const API = {
         if (message === "hi") resolve("Here I am  your mood guesser bot , I will send you jokes and memes which will make you happy!");
 
         else {
-          moodGuesser(message);
+          resolve(moodGuesser(message));
+          // moodGuesser(message);
           resolve("echo : " + message);
         }
       }, 2000);
